1a073130d6cdf8f5b444059111d828fe7f47dfa4
shaurya pratap rai <shauryaprataprai2007@gmail.com>
2026-01-20 15:38:38 +0530

    Implement main method to evaluate expressions

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter expression: ");
+        String expr = sc.nextLine();
+
+        double result = ExpressionEvaluator.evaluate(expr);
+        System.out.println("Result = " + result);
+    }
+}
